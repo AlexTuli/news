42a6ae571f21425d47db0d2de44fb7d11eae0f8b
Alexander <balexander1995@gmail.com>
2016-01-21 22:15:48 +0600

    Add SaveNews action

---
+++ src/main/java/com/epam/alex/action/SaveNews.java
@@ -0,0 +1,20 @@
+package com.epam.alex.action;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.springframework.web.struts.ActionSupport;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Alexander on 21.01.2016.
+ */
+public class SaveNews extends ActionSupport {
+
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return mapping.findForward("success");
+    }
+}
