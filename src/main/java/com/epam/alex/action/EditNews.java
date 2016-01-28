5c5313d4572cee92c32b7db78a50f0b429873712
Alexander <balexander1995@gmail.com>
2016-01-28 17:07:24 +0600

    Some bugs find

--- src/main/java/com/epam/alex/action/EditNews.java
+++ src/main/java/com/epam/alex/action/EditNews.java
@@ -1,8 +1,11 @@
 package com.epam.alex.action;
 
+import com.epam.alex.dao.NewsDao;
+import com.epam.alex.model.News;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.struts.ActionSupport;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,10 +19,20 @@ import javax.servlet.http.HttpServletResponse;
 
 public class EditNews extends ActionSupport {
 
+    private static final String ID = "id";
+    private static final String SUCCESS = "success";
+    private static final String NEWS = "news";
+
     @Override
     public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-        ViewNews viewNews = new ViewNews();
-        return viewNews.execute(mapping, form, request, response);
+        //todo Figure out how i Can getWebApplicationContext in ViewNews if I Create it from here
+        String id = request.getParameter(ID);
+        NewsDao newsDao = (NewsDao) getWebApplicationContext().getBean("newsDao");
+        News news = newsDao.readById(Integer.parseInt(id));
+        request.setAttribute(NEWS, news);
+        return mapping.findForward(SUCCESS);
+//        ViewNews viewNews = new ViewNews();
+
     }
 }
